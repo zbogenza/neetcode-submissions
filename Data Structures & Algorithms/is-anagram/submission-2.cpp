ed5dc976b8dc0db13809b7edc769f6f591cff096
Wojciech Starzyk <115113056+zbogenza@users.noreply.github.com>
2026-09-04 11:10:58 +0200

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        std::unordered_map<char, int> count;
+        for (int i = 0; i < s.length(); i++) count[s[i]]++;
+        for (int i = 0; i < t.length(); i++) {
+            if (--count[t[i]] < 0) return false;
+        }
+        return true;
+    }
+};
